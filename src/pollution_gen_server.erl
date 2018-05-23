2332c6c0d4a521212ba7b9a6918e93e96f66a49d
Rafał Mucha <6rafel6@gmail.com>
2018-05-23 13:01:03 +0200

    Completing pollution_gen_server

--- src/pollution_gen_server.erl
+++ src/pollution_gen_server.erl
@@ -13,11 +13,13 @@
 %% API
 -export([init/1, start_link/1, start/1, handle_call/3, addStation/2]).
 -export([addValue/4, removeValue/3, crash/0, handle_call/3]).
+-export([getDailyAverageDataCount/0, getDailyMean/2, getStationMean/2]).
+-export([getValue/3, handle_cast/2]).
 
 init(InitialValue) ->
   {ok, InitialValue}.
 
-start_link(InitialValue) ->
+start_link(_) ->
   gen_server:start_link(
     {local, pollutionServer},
     pollution_gen_server, pollution_var_server:getMonitor(), []
@@ -38,7 +40,19 @@ handle_call({addValue, [Name, Date, Type, Value]}, _From, Monitor) ->
 handle_call({removeValue, [Name, Date, Type]}, _From, Monitor) ->
   NewMonitor = pollution:removeValue(Monitor, Name, Date, Type),
   pollution_var_server:setMonitor(NewMonitor),
-  {reply, Monitor /= NewMonitor, NewMonitor}.
+  {reply, Monitor /= NewMonitor, NewMonitor};
+handle_call({getValue, [Name, Date, Type]}, _From, Monitor) ->
+  try pollution:getValue(Monitor, Name, Date, Type) of
+    Value -> {reply, Value, Monitor}
+  catch
+    error: _ -> {reply, -1, Monitor}
+  end;
+handle_call({getStationMean, [Name, Type]}, _From, Monitor) ->
+  {reply, pollution:getStationMean(Monitor, Name, Type), Monitor};
+handle_call({getDailyMean, [Date, Type]}, _From, Monitor) ->
+  {reply, pollution:getDailyMean(Monitor, Date, Type), Monitor};
+handle_call({getDailyAverageDataCount}, _From, Monitor) ->
+  {reply, pollution:getDailyAverageDataCount(Monitor), Monitor}.
 
 addStation(Name, Location) ->
   case gen_server:call(pollutionServer, {addStation, [Name, Location]}) of
@@ -58,10 +72,34 @@ removeValue(Name, Date, Type) ->
     false -> io:format("Cannot add~n")
   end.
 
+getValue(Name, Date, Type) ->
+  case gen_server:call(pollutionServer, {getValue, [Name, Date, Type]}) of
+    -1 -> io:format("Error, no such value~n"), -1;
+    Value ->  Value
+  end.
+
+getStationMean(Name, Type) ->
+  case gen_server:call(pollutionServer, {getStationMean, [Name, Type]}) of
+    -1 -> io:format("Error, no such value~n"), -1;
+    Value ->  Value
+  end.
+
+getDailyMean(Date, Type) ->
+  case gen_server:call(pollutionServer, {getDailyMean, [Date, Type]}) of
+    -1 -> io:format("Error, no such value~n"), -1;
+    Value ->  Value
+  end.
+
+getDailyAverageDataCount() ->
+  case gen_server:call(pollutionServer, {getStationMean}) of
+    -1 -> io:format("Error, no such value~n"), -1;
+    Value ->  Value
+  end.
+
 crash() ->
   gen_server:cast(pollutionServer, {crash}).
 
-handle_cast({crash}, Monitor) ->
+handle_cast({crash}, _) ->
   0 / 0.
 
 
